bd682de039c3dbb4c32ed3596407580a6e7a212c
Evan Mckenzie <615evan@gmail.com>
2022-08-29 11:14:18 -0400

    Done

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using PracticeLinq;
+
+
+var videoGames = new List<>();
+
+var gameOne = new VideoGames
+{
+    Name = "God Of War",
+    Rating = "M",
+    Price = 69.99
+};
+var gameTwo = new VideoGames { Name = "Horizon Forbidden West", Rating = "M", Price = 59.99 };
+var gameThree = new VideoGames
+{
+    Name = "Saints Row",
+    Rating = "M",
+    Price = 44.99
+};
+
+videoGames.Add(gameOne);
---
+++ PracticeLinq/VideoGames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    public class VideoGames
+    {
+        public VideoGames()
+        {
+
+        }
+        public string Name { get; set; }
+        public string Rating    { get; set; }
+        public double Price { get; set; }
+    }
+}
